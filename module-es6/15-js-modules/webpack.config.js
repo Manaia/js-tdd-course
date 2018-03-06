2c1a546e2d807cc70823856d14c1b41c4373d2c1
Manaia <aviana@neolaw.net.br>
2018-03-06 13:23:58 -0300

    criando variaveis ambiente no webpack

--- module-es6/15-js-modules/webpack.config.js
+++ module-es6/15-js-modules/webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-
+const nodeENV = process.env.NODE_ENV || 'production';
 module.exports = {
     entry: {
         filename: './app.js'
@@ -24,7 +24,11 @@ module.exports = {
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: false},
-            output: { comments: false }
+            output: { comments: false },
+            sourceMap: true
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {NODE_ENV: JSON.stringify(nodeENV)}
         })
     ]
 }
