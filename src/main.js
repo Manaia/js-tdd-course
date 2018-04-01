6fbb17d82b4f7304826d0f5dcfc4be8cd3cf853e
Arthur Manaia <arthurnovais7@gmail.com>
2018-04-01 01:08:30 -0300

    criando uma mini lib calculadora para praticar os testes

--- src/main.js
+++ src/main.js
@@ -3,9 +3,4 @@ const sub = (n1, n2) => n1 - n2;
 const mult = (n1, n2) => n1 * n2;
 const div = (n1, n2) => (n2 === 0) ? 'Não é possível divisão por zero!' : (n1 / n2);
 
-export {
-  sum,
-  sub,
-  mult,
-  div,
-}
+export { sum, sub, mult, div };
