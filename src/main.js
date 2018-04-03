da1e0dff4d91de005a47529aacf88137e12f1aad
Manaia <aviana@neolaw.net.br>
2018-04-03 13:19:54 -0300

    add check-coverage ao prepush hook

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 const FizzBuzz = (num) => {
-  if (num === 0 ) return 0;
+  if (num === 0) return 0;
   if ((num % 3 === 0) && (num % 5 === 0)) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
 
   return num;
 };
+
+function foo(){
+  return 'bla';
+}
 
 export default FizzBuzz;
