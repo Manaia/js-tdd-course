8afb5e8068a62a6d2552f8a549bb89b3e1d7a0bb
Manaia <aviana@neolaw.net.br>
2018-04-03 13:13:40 -0300

    adicionando a lib nyc para realizar os code coverage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const FizzBuzz = (num) => {
-  if((num % 3 === 0) && (num % 5 === 0)) return 'FizzBuzz';
-  if(num % 3 === 0) return 'Fizz';
-  if(num % 5 === 0) return 'Buzz';
+  if (num === 0 ) return 0;
+  if ((num % 3 === 0) && (num % 5 === 0)) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
 
   return num;
 };
